8c73621c2be18ebb21a891640f01b3c3f063e4b2
Matilde Morrone <contact@morrone.dev>
2024-05-14 20:38:41 +0200

    Add more compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.morrone.dev',
-	integrations: [mdx(), sitemap()],
+  site: 'https://blog.morrone.dev',
+  integrations: [mdx(), sitemap(), playformCompress()]
 });

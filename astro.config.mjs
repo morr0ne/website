31334e53938605506a9c385f6526aa4ecb90e010
Federico Maria Morrone <contact@morrone.dev>
2024-05-14 20:46:58 +0200

    Add tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import playformCompress from "@playform/compress";
-
 import playformInline from "@playform/inline";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blog.morrone.dev',
-	integrations: [mdx(), sitemap(), playformInline(), playformCompress(),]
+  site: 'https://blog.morrone.dev',
+  integrations: [tailwind(), mdx(), sitemap(), playformInline(), playformCompress()]
 });

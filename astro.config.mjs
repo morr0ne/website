39a44301cdaeb432732d43d7e73de85a71a68a57
Federico Maria Morrone <contact@morrone.dev>
2024-05-14 20:43:46 +0200

    Use critter to inline critical css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import playformCompress from "@playform/compress";
+
+import playformInline from "@playform/inline";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.morrone.dev',
-  integrations: [mdx(), sitemap(), playformCompress()]
+	site: 'https://blog.morrone.dev',
+	integrations: [mdx(), sitemap(), playformInline(), playformCompress(),]
 });

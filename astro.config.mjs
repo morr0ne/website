b82100e41da6eeec0c1fd92a5ffb7f2b687752c2
Federico Maria Morrone <contact@morrone.dev>
2024-05-20 23:08:08 +0200

    Use astro-icon and iconify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import playformCompress from '@playform/compress';
 import playformInline from '@playform/inline';
-
 import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.morrone.dev',
   integrations: [
+    icon(),
     tailwind(),
     mdx(),
     sitemap(),
